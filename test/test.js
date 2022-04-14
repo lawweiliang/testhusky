6211813668392f4fdcac044bf4f3fe765b5f8826
liang <yeslaw90@hotmail.com>
2022-04-14 20:59:02 +0800

    update file

--- test/test.js
+++ test/test.js
@@ -8,8 +8,9 @@ let account2;
 let erc721RExample;
 
 const MINT_PRICE = "0.1";
+const malays = "2.5";
 
-describe("ERC721RExample', function () {
+describe("ERC721RExample", function () {
   before(async function () {
     [owner, account2, account3, account4] = await ethers.getSigners();
     const ERC721RExample = await ethers.getContractFactory("ERC721RExample");
